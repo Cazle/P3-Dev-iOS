775d1839304c3101cf0007ce588752cad20407a8
Cazle <kykyglt@gmail.com>
2023-04-19 16:00:29 +0200

    Making team, test 1

--- Game/main.swift
+++ Game/main.swift
@@ -42,14 +42,45 @@ class Game {
 
 class GameSession {
     
+    var playerOne = Player(name: "Player one")
+    var playerTwo = Player(name: "Player two")
     
-    func makeATeam(){
-        print("Welcome, make a team please")
+    
+func makeATeam(){
+    
+    while playerOne.heroes.count <= 2{
+        print("""
+ Now, you have to make a team of three heroes.
+ 1 : Warrior
+ 2 : Wizard
+ 3 : Thief
+ 4 : Dwarf
+ 5 : Healer
+ """)
+        let chooseHero = readLine()
         
-        var players = Player(name: "First Player", name: "Second Player")
-       
+        switch chooseHero{
+                
+        case "1": playerOne.heroes.append(Warrior())
+            print("You chose Warrior.")
+                
+        case "2": playerOne.heroes.append(Wizard())
+            print("You chose Wizard.")
+
+        case "3": playerOne.heroes.append(Thief())
+            print("You chose Thief.")
+                
+        case "4": playerOne.heroes.append(Dwarf())
+            print("You chose Dwarf.")
+            
+        case "5":playerOne.heroes.append(Healer(heal: 20))
+            print("You chose Healer.")
+                
+        default: print("You must select between 1 and 5.")
+            }
+        }
     }
-    
 }
+
 
 
