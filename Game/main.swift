1a150e7ad52ada705327ef27551efcdcd194ee7d
Cazle <kykyglt@gmail.com>
2023-04-25 16:56:22 +0200

    Added : Security for the name input (same input, or empty input)

--- Game/main.swift
+++ Game/main.swift
@@ -1,12 +1,12 @@
 import Foundation
-
+// Few declarations for using my classes. "game" is for launching the game, and "hero", is used for naming my characters.
 let game = Game()
 let hero = Hero(weapons: "", healthPoints: 0, attack: 0)
-
 game.startingTheGame()
 
 class Game {
     
+    // Main menu, with 2 options, one to start the game, the other to exit the game.
     func startingTheGame(){
     print("Welcome to the new game of FrenchGame company !")
     print("""
@@ -14,9 +14,9 @@ class Game {
      1 : Start game
      2 : Quit game
     """)
+        
         let startMenu = readLine()
-            
-        switch startMenu{
+            switch startMenu{
             
         case "1" :
             let chooseTeam = GameSession()
@@ -34,23 +34,40 @@ class Game {
 }
 class GameSession {
     
+    // Declare two players with both name
     var playerOne = Player(name: "Player one")
     var playerTwo = Player(name: "Player two")
     
+    // Function to let both players choose their heroes name for each team.
     func herosNames(){
-        print("\(playerOne.name), enter 3 names for your characters.")
-        
-        while hero.names.count <= 5{
-            let names = readLine()
-            hero.names.append(names!)
-        if hero.names.count == 3{
-            print("\(playerTwo.name), enter 3 names for you characters.")
+            print("\(playerOne.name), enter 3 names for your characters.")
+            while hero.names.count <= 2 {
+                guard let name = readLine(), !name.isEmpty else {
+                    print("Please enter a valid name.")
+                    continue
+                }
+                if hero.names.contains(name){
+                    print("Please, choose an other name")
+                    continue
+                }
+                hero.names.append(name)
             }
-        }
+            print("\(playerTwo.name), enter 3 names for your characters.")
+            while hero.names.count <= 5 {
+                guard let name = readLine(), !name.isEmpty else {
+                    print("Please enter a valid name.")
+                    continue
+                }
+                if hero.names.contains(name){
+                    print("Please, choose an other name")
+                    continue
+                }
+                hero.names.append(name)
+            }
         print("\(hero.names[0]), \(hero.names[1]), \(hero.names[2]), \(hero.names[3]), \(hero.names[4]), \(hero.names[5])")
 }
 
-    
+    // Player one has too choose 3 heroes, and he has 5 options. (Each class of the game)
     func makeTeamOne(){
         herosNames()
         while playerOne.heroes.count <= 2{
@@ -87,6 +104,7 @@ class GameSession {
         }
     makeTeamTwo()
 }
+    // Player Two has too choose 3 heroes, and he has 5 options. (Each class of the game)
     func makeTeamTwo(){
         
         while playerTwo.heroes.count <= 2 {
@@ -122,6 +140,7 @@ class GameSession {
                     default: print("You must select between 1 and 5.")
                 }
             }
+        // Now we indicate to both players, what heroes they have and how they are named.
         print("\(playerOne.name), you have a \(type(of: playerOne.heroes[0])) named \(hero.names[0]), a \(type(of: playerOne.heroes[1])) named \(hero.names[1]), and a \(type(of: playerOne.heroes[2])) named \(hero.names[2]).")
         print("\(playerTwo.name), you have a \(type(of: playerTwo.heroes[0])) named \(hero.names[3]), a \(type(of: playerTwo.heroes[1])) named \(hero.names[4]), and a \(type(of: playerTwo.heroes[2])) named \(hero.names[5]).")
     }
