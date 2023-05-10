c6a55981627c023fda6792a9418c0c5a43651426
Cazle <kykyglt@gmail.com>
2023-05-10 14:03:51 +0200

    Bug fixe healer can now deal damage, project finished, waiting mentoring session

--- Game/Characters/Healer.swift
+++ Game/Characters/Healer.swift
@@ -4,6 +4,6 @@ class Healer: Character{
     
     
     init(name: String) {
-        super .init(name: name, weapons: "Book of Wisdom", healthPoints: 70, damage: 0, heal: 30)
+        super .init(name: name, weapons: "Book of Wisdom", healthPoints: 70, damage: 10, heal: 30)
     }
 }
